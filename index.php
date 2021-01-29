6b414b96607a6ccc4813a44a541ee420c67b2dda
Clems-web <clement.servais1@gmail.com>
2021-01-29 12:47:28 +0100

    terminé

--- index.php
+++ index.php
@@ -5,14 +5,21 @@
  * 2. Afficher le contenu du fichier dans un div
  */
 // TODO Votre code ici.
-
+$contents = file_get_contents("lire.txt");
+echo $contents . "<br>";
+echo "<br>";
 /**
  * 3. Faites la même chose à l'aide d'une boucle et de la fonction fgets(), récupérez les lignes du fichier une à une.
  * 4. Affichez le contenu de chaque ligne dans un nouveau span.
  * --> N'oubliez pas de fermer votre fichier.
  */
 // TODO Votre code ici.
-
+$file = fopen("lire.txt", "rb");
+while ($ligne = fgets($file)) {
+    echo "<span>". nl2br($ligne). "</span>";
+    fclose($file);
+    echo "<br>";
+}
 
 /**
  * 5. Récupérez les caractères un à un et n'affichez le caractère que s'il s'agit d'autre chose qu'un espace, qu'un point ou qu'un point d'interrogation / exclamation.
@@ -20,6 +27,16 @@
  */
 // TODO Votre code ici.
 
+while ($char = fgetc($file)) {
+
+    if ($char === " " || $char === "." || $char === "?" || $char === "!") {
+        echo "";
+    }
+    else {
+        echo $char;
+    }
+    fclose($file);
+}
 
 /**
  * 6. Ne cherchez pas la logique dans cet exo ^^
